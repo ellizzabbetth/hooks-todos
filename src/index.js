ae852f1178f50c95212480d795f51124513e182a
elizabeth <ellizzabbetth@gmail.com>
2019-05-07 06:12:27 -0400

    more from client

--- src/index.js
+++ src/index.js
@@ -1,18 +1,55 @@
-import React from 'react';
+import React, { useContext, useReducer, useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import TodosContext from './context';
+import todosReducer from './reducer';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
+import axios from 'axios';
 
 
-export const UserContext = React.createContext();
+// each hook manages it's own state
+const useAPI = endpoint => {
+  // set initial state to empty array
+  const [data, setData] = useState([]);
+  // call getData when App component gets mounted
+  useEffect(() => {
+      getData();
+  }, []);
+  // to make the requests for todos.
+  const getData = async () => {
+    const response = await axios.get(endpoint);
+    setData(response.data);
+  }
+  // return todos that's being set in setData
+  return data;
+}
 
-const username = "Dave";
+const App =() => {
+   const initialState = useContext(TodosContext);
+   const [state, dispatch] = useReducer(todosReducer, initialState);
+   // custom hook
+   const savedTodos =  useAPI("https://hooks-api.ellizzabbetth.now.sh/todos");
+   // put saved todos in state using redux
+   useEffect(() => {
+     // dispatch new action
+     dispatch({
+       type: "GET_TODOS",
+       payload: savedTodos
+     }) // dispatch action when savedTodos changes
+   }, [savedTodos]);
+
+   return (
+     <TodosContext.Provider value={{state, dispatch}}>
+      <TodoForm />
+      <TodoList />
+     </TodosContext.Provider>
+   )
+}
 
 ReactDOM.render(
-  <UserContext.Provider value = {username}>
+
     <App />
-  </UserContext.Provider>
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
